4f75d64d49dc5ade59e2fdea08bab3c0c935e8a0
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-01-20 14:19:43 +0700

    Add navigation folder

---
+++ src/navigation/index.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import LoginScreen from '../srceens/LoginScreen';
+import HomeScreen from '../srceens/HomeScreen';
+import AuthScreen from '../srceens/AuthScreen';
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Auth"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Auth" component={AuthScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigation;
--- src/srceens/HomeScreen/index.tsx
+++ src/srceens/HomeScreen/index.tsx
@@ -2,15 +2,8 @@ import React from 'react';
 
 import {View} from 'react-native';
 import {Text} from 'react-native-paper';
-import {useSelector} from 'react-redux';
 
 function HomeScreen() {
-  const isAuthenticated = useSelector(
-    (state: any) => state.auth.isAuthenticated,
-  );
-
-  console.log('IS AUTH =================', isAuthenticated);
-
   return (
     <View>
       <Text>Home</Text>
