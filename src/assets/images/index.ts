2ac21fa94ff684dbf0c5ab64e78b44482d2e89cd
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-01-20 15:24:17 +0700

    Bottom bar navigation

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,14 @@
 export const logo = require('./logo.png');
 export const eye = require('./eye-enable.png');
 export const eyeOff = require('./eye-disable.png');
+export const home = require('./home-icon.png');
+export const homeActive = require('./home-active-icon.png');
+
+export const user = require('./user-icon.png');
+export const userActive = require('./user-active-icon.png');
+
+export const chart = require('./chart-icon.png');
+export const chartActive = require('./chart-active-icon.png');
+
+export const store = require('./store-icon.png');
+export const storeActive = require('./store-active-icon.png');
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,8 +3,8 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 
 import LoginScreen from '../srceens/LoginScreen';
-import HomeScreen from '../srceens/HomeScreen';
 import AuthScreen from '../srceens/AuthScreen';
+import TabNavigator from './TabNavigator';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +15,7 @@ function AppNavigation() {
         initialRouteName="Auth"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Auth" component={AuthScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={TabNavigator} />
         <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
