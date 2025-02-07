b8846b8a20a9a54836419c2056bf59a986c803d7
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-02-07 14:41:56 +0700

    Define shopping screen

---
+++ src/srceens/ShoppingScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {View} from 'react-native';
+import {ShoppingProps} from '../../types';
+
+const ShoppingScreen: React.FC<ShoppingProps> = () => {
+  return (
+    <View>
+      <Text>Shopping Screen</Text>
+    </View>
+  );
+};
+
+export default ShoppingScreen;
