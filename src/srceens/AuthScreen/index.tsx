d3eda5ab92ec97e68b20c92153c3fc8f19c574ea
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-03-05 11:41:01 +0700

    Holding state user

--- src/srceens/AuthScreen/index.tsx
+++ src/srceens/AuthScreen/index.tsx
@@ -1,39 +1,39 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {useDispatch} from 'react-redux';
 import {login} from '../../redux/slices/authSlice';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {setUser} from '../../redux/slices/userSlice';
-import {UserType} from '../../types';
+import {RootStackParamList, UserType} from '../../types';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 function AuthScreen() {
-  const navigation: any = useNavigation();
-
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const checkAuthState = async () => {
-      const isAuthenticated: string | null = await AsyncStorage.getItem(
-        'isAuthenticated',
-      );
+      const isAuthenticated = await AsyncStorage.getItem('isAuthenticated');
+      const data = await AsyncStorage.getItem('user');
 
-      const data: string | null = await AsyncStorage.getItem('user');
-
-      if (isAuthenticated) {
-        if (data) {
-          const user: UserType = JSON.parse(data);
-
-          dispatch(login());
-          dispatch(setUser(user));
-          navigation.replace('Home');
-        }
+      if (isAuthenticated && data) {
+        const user: UserType = JSON.parse(data);
+        dispatch(login());
+        dispatch(setUser(user));
+        navigation.replace('Home');
       } else {
         navigation.replace('Login');
       }
+
+      setIsLoading(false);
     };
 
     checkAuthState();
   }, [dispatch, navigation]);
+
+  if (isLoading) return null;
 
   return null;
 }
